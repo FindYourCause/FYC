96c5274fdb650b4923690e27e584e82fad25d191
azgar <azgarmd957@gmail.com>
2018-11-08 17:28:37 +0530

    c

--- FYC.DataAccess/EFRepository/FYC_DEF_Category.cs
+++ FYC.DataAccess/EFRepository/FYC_DEF_Category.cs
@@ -6,14 +6,8 @@ namespace FYC.DataAccess.EFRepository
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
-    public partial class FYC_DEF_Category
+    public partial class Fyc_Def_Category
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public FYC_DEF_Category()
-        {
-            FYC_TRAN_Cause = new HashSet<FYC_TRAN_Cause>();
-        }
-
         [Key]
         public long CategoryID { get; set; }
 
@@ -34,8 +28,5 @@ namespace FYC.DataAccess.EFRepository
 
         [Column(TypeName = "date")]
         public DateTime? ModifiedDate { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<FYC_TRAN_Cause> FYC_TRAN_Cause { get; set; }
     }
 }
